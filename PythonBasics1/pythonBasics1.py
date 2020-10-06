39664c9ffe385db22ef1f69869acc748fa59089b
Cindy <cguy9@uncc.edu>
2020-10-05 23:04:44 -0400

    PythonBasics 1 - added logic to starts_with_vowel function

--- PythonBasics1/pythonBasics1.py
+++ PythonBasics1/pythonBasics1.py
@@ -1,12 +1,12 @@
 # Python Activtiy
-# 
-# Fill in the code for the functions below. 
+#
+# Fill in the code for the functions below.
 # The starter code for each function includes a 'return'
 # which is just a placeholder for your code.
 
 # Part A. starts_with
 # Define a function starts_with(s, char) that takes a string and a character
-# and returns true if the string starts with that character and false otherwise. 
+# and returns true if the string starts with that character and false otherwise.
 def starts_with(s, char):
     # YOUR CODE HERE
     #check to see if either parameter is empty
@@ -25,13 +25,34 @@ def starts_with(s, char):
 
 # Part B. starts_with_vowel
 # Define a function starts_with_vowel(s) that takes a string and
-# returns true if the string starts with a vowel and false otherwise. 
+# returns true if the string starts with a vowel and false otherwise.
 # For our purposes, a consonant is any letter other than A, E, I, O, U)
-# Your solution should work for both upper and lower cases 
+# Your solution should work for both upper and lower cases
 def starts_with_vowel(s):
     # YOUR CODE HERE
-
-    return
+    #check to see if the string is empty
+    if s == '':
+        return False
+    else:
+        #else the string contains letters and needs to be checked
+        num = ord(s[0])
+        #A
+        if num == 65 or num == 97:
+            return True
+        #E
+        elif num == 69 or num == 101:
+            return True
+        #I
+        elif num == 73 or num == 105:
+            return True
+        #O
+        elif num == 79 or num == 111:
+            return True
+        #U
+        elif num == 85 or num == 117:
+            return True
+        else:
+            return False
 
 
 # Part C. max_min_sum
