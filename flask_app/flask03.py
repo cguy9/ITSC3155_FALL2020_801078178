caff02b723fbbc834ee284667ed5dfe10c5d1ca0
Cindy <cguy9@uncc.edu>
2020-11-14 21:12:53 -0500

    flask app #3 - capturing form data in POST request

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -42,7 +42,8 @@ def new_note():
     a_user = {'name': 'Cynthia', 'email':'cguy9@uncc.edu'}
 
     if request.method == 'POST':
-        return '<h1> POST method used for this request </h1>'
+        request_data = request.form
+        return f"data: {request_data} !"
     else:
         return render_template('new.html', user=a_user)
 
