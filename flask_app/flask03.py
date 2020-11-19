b553a2b54552c3d0c468ab1b9a43a520b506b881
Cindy <cguy9@uncc.edu>
2020-11-18 22:41:57 -0500

    flask app #3: - saving note to mock list of notes

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -1,12 +1,19 @@
 # FLASK Tutorial 1 -- We show the bare bones code to get an app up and running
 
 # imports
-import os                 # os is used to get environment variables IP & PORT
-from flask import Flask
-from flask import render_template   # Flask is the web app that we will customize
+import os  # os is used to get environment variables IP & PORT
+from flask import Flask  # Flask is the web app that we will customize
+from flask import render_template
 from flask import request
+from flask import redirect, url_for
 
-app = Flask(__name__)     # create an app
+app = Flask(__name__)  # create an app
+
+notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
+         2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
+         3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
+         }
+
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
@@ -14,40 +21,50 @@ app = Flask(__name__)     # create an app
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user = {'name': 'Cynthia', 'email':'cguy@uncc.edu'}
-    return render_template('index.html', user = a_user)
+    a_user = {'name': 'Cynthia', 'email': 'cguy9@uncc.edu'}
+    return render_template("index.html", user=a_user)
+
 
 @app.route('/notes')
-def notes():
-    notes = {1: {'title':'First note', 'texr':'This is my first note', 'date':'10-1-2020'},
-             2: {'title':'Second note', 'texr':'This is my second note', 'date':'10-2-2020'},
-             3: {'title':'Third note', 'text':'This is my third note', 'date':'10-3-2020'}
-             }
-    a_user = {'name': 'Cynthia', 'email':'cguy9@uncc.edu'}
-    return render_template('notes.html', notes=notes, user=a_user)
+def get_notes():
+
+    a_user = {'name': 'Cynthia', 'email': 'cguy9@uncc.edu'}
+    return render_template("notes.html", notes=notes, user=a_user)
+
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    notes = {1: {'title':'First note', 'text':'This is my first note', 'date':'10-1-2020'},
-             2: {'title':'Second note', 'text':'This is my second note', 'date':'10-2-2020'},
-             3: {'title':'Third note', 'text':'This is my third note', 'date':'10-3-2020'}
-             }
-    a_user = {'name': 'Cynthia', 'email':'cguy9@uncc.edu'}
-    return render_template('note.html', note=notes[int(note_id)], user=a_user)
+    a_user = {'name': 'Cynthia', 'email': 'cguy9@uncc.edu'}
+    return render_template("note.html", note=notes[int(note_id)], user=a_user)
+
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
+    a_user = {'name': 'Cynthia', 'email': 'cguy9@uncc.edu'}
+
     # check method used for request
-    print('request method is', request.method)
-    a_user = {'name': 'Cynthia', 'email':'cguy9@uncc.edu'}
+    if request.method == "POST":
+        # get title data
+        title = request.form["title"]
+        # get note data
+        text = request.form["noteText"]
+        # create date stamp
+        from datetime import date
+        today = date.today()
+        # format date mm/dd/yyyy
+        today = today.strftime("%m-%d-%y")
+        # get the last ID used and increment by 1
+        id = len(notes)+1
+        # create new note entry
+        notes[id] = {"title": title, "text": text, "date": today}
 
-    if request.method == 'POST':
-        request_data = request.form
-        return f"data: {request_data} !"
+        return redirect(url_for('get_notes', name=a_user))
+
     else:
-        return render_template('new.html', user=a_user)
+        return render_template("new.html", user=a_user)
 
-app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
+
+app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
 
 # To see the web page in your web browser, go to the url,
 #   http://127.0.0.1:5000
