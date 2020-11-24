2355959ce8bf563d2abdeea625f016d3ebaa4fbe
Cindy <cguy9@uncc.edu>
2020-11-24 17:11:02 -0500

    flask app #4 - db queries

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -7,6 +7,8 @@ from flask import render_template
 from flask import request
 from flask import redirect, url_for
 from database import db
+from models import Note as Note
+from models import User as User
 
 app = Flask(__name__)  # create an app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
@@ -31,26 +33,33 @@ notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user = {'name': 'Cynthia', 'email': 'cguy9@uncc.edu'}
+    # get user from database
+    a_user = db.session.query(User).filter_by(email="cguy9@uncc.edu")
     return render_template("index.html", user=a_user)
 
 
 @app.route('/notes')
 def get_notes():
+    # retrieve user from database
+    a_user = db.session.query(User).filter_by(email="cguy9@uncc.edu")
+    # retrieve notes from database
+    my_notes = db.session.query(Note).all()
 
-    a_user = {'name': 'Cynthia', 'email': 'cguy9@uncc.edu'}
-    return render_template("notes.html", notes=notes, user=a_user)
+    return render_template("notes.html", notes=my_notes, user=a_user)
 
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = {'name': 'Cynthia', 'email': 'cguy9@uncc.edu'}
-    return render_template("note.html", note=notes[int(note_id)], user=a_user)
+    # retrieve user from database
+    a_user = db.session.query(User).filter_by(email="cguy9@uncc.edu")
+    # retrieve notes from database
+    my_note = db.session.query(Note).filter_by(id=note_id)
+    return render_template("note.html", note=my_note, user=a_user)
 
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
-    a_user = {'name': 'Cynthia', 'email': 'cguy9@uncc.edu'}
+    a_user = db.session.query(User).filter_by(email="cguy9@uncc.edu")
 
     # check method used for request
     if request.method == "POST":
@@ -62,15 +71,15 @@ def new_note():
         from datetime import date
         today = date.today()
         # format date mm/dd/yyyy
-        today = today.strftime("%m-%d-%y")
-        # get the last ID used and increment by 1
-        id = len(notes)+1
-        # create new note entry
-        notes[id] = {"title": title, "text": text, "date": today}
+        today = today.strftime("%m-%d-%Y")
+        new_record = Note(title, text, today)
+        db.session.add(new_record)
+        db.session.commit()
 
-        return redirect(url_for('get_notes', name=a_user))
+        return redirect(url_for('get_notes'))
 
     else:
+        # GET request - show new note form
         return render_template("new.html", user=a_user)
 
 
