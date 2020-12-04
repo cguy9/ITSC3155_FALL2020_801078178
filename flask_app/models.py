05e8890e7bbc7f3a9f249488085df140b95f2aa1
Cindy <cguy9@uncc.edu>
2020-12-04 16:32:05 -0500

    flask app #6 - created inital app file

--- flask_app/models.py
+++ flask_app/models.py
@@ -19,3 +19,15 @@ class User(db.Model):
     def __init__(self, name, email):
         self.name = name
         self.email = email
+
+
+class Comment(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    title = db.Column("title", db.String(200))
+    text = db.Column("text", db.String(100))
+    date = db.Column("date", db.String(50))
+
+    def __init__(self, title, text, date):
+        self.title = title
+        self.text = text
+        self.date = date
