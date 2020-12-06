446330db2f7ff7e6d26df9e7a9475e24f04071b9
Cindy <cguy9@uncc.edu>
2020-12-06 18:50:58 -0500

    added registration functionality

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -3,6 +3,7 @@
 # imports
 import os  # os is used to get environment variables IP & PORT
 
+import bcrypt
 from flask import Flask  # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
@@ -11,10 +12,13 @@ from database import db
 from models import Note as Note
 from models import User as User
 from models import Comment as Comment
+from forms import RegisterForm
+from flask import session
 
 app = Flask(__name__)  # create an app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'SE3155'
 
 # Bind SQLAlchemy db object to this Flask app
 db.init_app(app)
@@ -36,18 +40,21 @@ notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10
 @app.route('/index')
 def index():
     # get user from database
-    a_user = db.session.query(User).filter_by(email="cguy9@uncc.edu").one()
-    return render_template("index.html", user=a_user)
+    #a_user = db.session.query(User).filter_by(email="cguy9@uncc.edu").one()
+    return render_template("index.html", user=session['user'])
 
 
 @app.route('/notes')
 def get_notes():
     # retrieve user from database
-    a_user = db.session.query(User).filter_by(email="cguy9@uncc.edu").one()
-    # retrieve notes from database
-    my_notes = db.session.query(Note).all()
+    # check if a user is saved in session
+    if session.get('user'):
+        # retrieve notes from database
+        my_notes = db.session.query(Note).filter_by(user_id=session['user_id']).all()
 
-    return render_template("notes.html", notes=my_notes, user=a_user)
+        return render_template("notes.html", notes=my_notes, user=session['user'])
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/notes/<note_id>')
@@ -119,6 +126,32 @@ def delete_note(note_id):
     db.session.commit()
 
     return redirect(url_for('get_notes'))
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+
+    # validate_on_submit only validates using POST
+    if form.validate_on_submit():
+        # form validation included a criteria to check the username does not exist
+        # we can know that we are safe to add the user to the database
+        password_hash = bcrypt.hashpw(
+            request.form['password'].encode('utf-8'), bcrypt.gensalt()
+        )
+        first_name = request.form['firstname']
+        last_name = request.form['lastname']
+        new_record = User(first_name, last_name, request.form['email'], password_hash)
+        db.session.add(new_record)
+        db.session.commit()
+        # save the user's name to session
+        session['user'] = first_name
+        # get the id of the newly created database record
+        the_user = db.session.query(User).filter_by(email=request.form['email']).one()
+        # save the user's id to the session
+        session['user_id'] = the_user.id
+
+        return redirect(url_for('get_notes'))
+    return render_template('register.html', form=form)
 
 @app.route('/notes/comment/<note_id>')
 def comment(note_id):
